963e019680bbdd971e5cc446e3ad47f19e59b27d
Tjerk Woudsma <tjerkwoudsma@gmail.com>
2020-10-09 04:33:20 +0200

    fix: use algolia staging

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const SWARMLET = "Swarmlet";
 const SWARMLET_TAGLINE = "A self-hosted, open-source Platform as a Service";
-const SWARMLET_URL = "https://swarmlet.dev";
+const SWARMLET_URL = "https://staging.swarmlet.dev";
 const SWARMLET_REPO_URL = "https://github.com/swarmlet/swarmlet";
 const SWARMLET_REMOTE_URL = `git@github.com:swarmlet/swarmlet.git`;
 const SWARMLET_KEYWORDS = [
@@ -142,7 +142,7 @@ module.exports = {
   themeConfig: {
     algolia: {
       apiKey: "2bc24cfb86b5e86e69a4fa57c1206102",
-      indexName: "docs-production",
+      indexName: "docs-staging",
       appId: "NI8OZVGMZU",
     },
     navbar: {
